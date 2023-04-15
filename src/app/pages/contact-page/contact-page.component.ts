7e9816fb376ba7a9b9d136eaf7667fb10de6f012
Patrice-H <webmestre64@gmail.com>
2023-04-15 22:30:27 +0200

    Add contact page

---
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact-page',
+  templateUrl: './contact-page.component.html',
+  styleUrls: ['./contact-page.component.scss'],
+})
+export class ContactPageComponent {
+  formContainer = new FormGroup({
+    firstName: new FormControl(null, []),
+    lastName: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[A-Za-z]+$/),
+    ]),
+    companyName: new FormControl(null, []),
+    eMail: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+    ]),
+    subject: new FormControl(null, []),
+    message: new FormControl(null, [Validators.maxLength(255)]),
+  });
+
+  onSubmitForm(): void {
+    console.log(this.formContainer.value);
+  }
+}
