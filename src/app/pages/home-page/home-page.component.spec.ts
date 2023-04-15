1886d817268d529f576226d09e61144292435814
Patrice-H <webmestre64@gmail.com>
2023-04-15 13:30:07 +0200

    Fixes issue#1 Tests failed

--- src/app/pages/home-page/home-page.component.spec.ts
+++ src/app/pages/home-page/home-page.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { render, screen } from '@testing-library/angular';
 
 import { HomePageComponent } from './home-page.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
@@ -10,6 +11,7 @@ describe('HomePageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomePageComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePageComponent);
