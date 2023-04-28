4cc9f9ba000bcf9d971517dfa8a3a80b682db01a
Patrice-H <webmestre64@gmail.com>
2023-04-28 12:10:59 +0200

    Update modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,15 @@ import { ContactPageComponent } from './pages/contact-page/contact-page.componen
     HomePageComponent,
     NavbarComponent,
     ContactPageComponent,
+    SkillsPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
