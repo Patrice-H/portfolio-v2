b1fadd5befc2774e1cfecaa0b0dabc31802d5856
Patrice-H <webmestre64@gmail.com>
2023-05-09 11:01:59 +0200

    Update tests

--- src/app/pages/home-page/home-page.component.spec.ts
+++ src/app/pages/home-page/home-page.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { render, screen } from '@testing-library/angular';
-
+import { screen } from '@testing-library/angular';
 import { HomePageComponent } from './home-page.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
@@ -19,21 +18,21 @@ describe('HomePageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('UI tests suite', () => {
+    it('should create component', () => {
+      expect(component).toBeTruthy();
+    });
 
-  /*** Integrity of page tests ***/
+    it('shoud render title', () => {
+      expect(screen.getByText(/^Patrice-H/)).toBeDefined();
+    });
 
-  it('shoud render title', () => {
-    expect(screen.getByText(/^Patrice-H/)).toBeDefined();
-  });
+    it('shoud render slogan', () => {
+      expect(screen.getByText(/^Construisons ensemble/)).toBeDefined();
+    });
 
-  it('shoud render slogan', () => {
-    expect(screen.getByText(/^Construisons ensemble/)).toBeDefined();
-  });
-
-  it('shoud render image', () => {
-    expect(screen.getByAltText('main tendue')).toBeDefined();
+    it('shoud render image', () => {
+      expect(screen.getByAltText('main tendue')).toBeDefined();
+    });
   });
 });
