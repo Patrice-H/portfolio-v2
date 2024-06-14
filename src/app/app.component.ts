e967beae9faf77da366b00c93433621ac7600b89
Patrice-H <webmestre64@gmail.com>
2024-06-14 13:10:40 +0200

    Updates App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HomePageComponent } from './page/home-page/home-page.component';
+import { ContactPageComponent } from './page/contact-page/contact-page.component';
+import { AboutPageComponent } from './page/about-page/about-page.component';
+import { ProjectsPageComponent } from './page/projects-page/projects-page.component';
+import { MainMenuComponent } from './component/main-menu/main-menu.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomePageComponent,
+    ContactPageComponent,
+    AboutPageComponent,
+    ProjectsPageComponent,
+    MainMenuComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'portfolio-V2';
+  title = 'portfolio';
 }
