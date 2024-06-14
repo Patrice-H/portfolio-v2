a1637f5e7b56b110641d496751cdf74a30c7f9f1
Patrice-H <webmestre64@gmail.com>
2024-06-14 13:10:21 +0200

    Config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './page/home-page/home-page.component';
+import { ContactPageComponent } from './page/contact-page/contact-page.component';
+import { AboutPageComponent } from './page/about-page/about-page.component';
+import { ProjectsPageComponent } from './page/projects-page/projects-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'projects', component: ProjectsPageComponent },
+];
