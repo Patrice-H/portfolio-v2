ef075e4ef5d0c376c1d016be931dcb6cdcfed47b
Patrice-H <webmestre64@gmail.com>
2024-06-14 13:09:06 +0200

    Updates contact page

--- src/app/page/contact-page/contact-page.component.ts
+++ src/app/page/contact-page/contact-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from '../../component/contact-form/contact-form.component';
+import { SocialNetworksComponent } from '../../component/social-networks/social-networks.component';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent, SocialNetworksComponent],
   templateUrl: './contact-page.component.html',
-  styleUrl: './contact-page.component.scss'
+  styleUrl: './contact-page.component.scss',
 })
-export class ContactPageComponent {
-
-}
+export class ContactPageComponent {}
