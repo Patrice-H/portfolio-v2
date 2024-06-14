dd0081f733c751ac7f3300552f0615c4b44fd18a
Patrice-H <webmestre64@gmail.com>
2024-06-14 13:08:33 +0200

    Add social network block

---
+++ src/app/component/social-networks/social-networks.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-networks',
+  standalone: true,
+  imports: [],
+  templateUrl: './social-networks.component.html',
+  styleUrl: './social-networks.component.scss',
+})
+export class SocialNetworksComponent {
+  email: String = '@';
+
+  openNewPage(link: string) {
+    window.open(link);
+  }
+}
