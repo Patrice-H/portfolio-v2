260d0abb1cfe550cadf4239049b6d728a24982ee
Patrice-H <webmestre64@gmail.com>
2024-06-30 20:06:38 +0200

    Updates Button row component

--- src/app/component/button-row/button-row.component.ts
+++ src/app/component/button-row/button-row.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CustomButtonComponent } from '../custom-button/custom-button.component';
 
 @Component({
   selector: 'app-button-row',
   standalone: true,
-  imports: [CustomButtonComponent],
+  imports: [CustomButtonComponent, CommonModule],
   templateUrl: './button-row.component.html',
   styleUrl: './button-row.component.scss',
 })
-export class ButtonRowComponent {}
+export class ButtonRowComponent implements OnInit {
+  @Input() number_of_buttons!: number;
+  @Output() contentChange: EventEmitter<number> = new EventEmitter();
+  buttons!: any[];
+  content!: number;
+
+  ngOnInit(): void {
+    this.buttons = new Array(this.number_of_buttons);
+    this.content = 0;
+    this.buttons[0] = 'active-btn';
+  }
+
+  displayContent(n: number) {
+    this.content = n;
+    this.contentChange.emit(this.content);
+    this.resetButtons();
+    const id = 'frame-' + n;
+    const button = document.getElementById(id)
+    button?.classList.add('active-btn');
+  }
+
+  resetButtons() {
+    for(let i = 0; i < 4; i++) {
+      const id = 'frame-' + i;
+      const button = document.getElementById(id)
+      button?.classList.remove('active-btn');
+    }
+  }
+}
