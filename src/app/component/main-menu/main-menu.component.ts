ec1e13e70d00804914a9821c8435a4314cadf264
Patrice-H <webmestre64@gmail.com>
2024-07-25 21:25:55 +0200

    Updates component

--- src/app/component/main-menu/main-menu.component.ts
+++ src/app/component/main-menu/main-menu.component.ts
@@ -1,23 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, MatButtonModule],
+  imports: [RouterLink, RouterLinkActive, MatButtonModule, CommonModule],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss',
 })
-export class MainMenuComponent {
-  homeBtnLabel: String = 'Accueil';
-  aboutBtnLabel: String = 'A propos';
-  projectsBtnLabel: String = 'Projets';
-  contactBtnLabel: String = 'Contact';
+export class MainMenuComponent implements OnInit {
+  tabs: any[] = [
+    {
+      id: 'home-btn',
+      link: '/',
+      label: 'Accueil',
+      active: true,
+      page: 'home-page',
+    },
+    {
+      id: 'about-btn',
+      link: '/about',
+      label: 'A propos',
+      active: false,
+      page: 'about-page',
+    },
+    {
+      id: 'contact-btn',
+      link: '/contact',
+      label: 'Contact',
+      active: false,
+      page: 'contact-page',
+    },
+    {
+      id: 'projects-btn',
+      link: '/projects',
+      label: 'Projets',
+      active: false,
+      page: 'projects-page',
+    },
+  ];
+  displayedMenu: Boolean = false;
+  disabledButton: Boolean = true;
 
-  displayMenu() {
-    let elmt = document.getElementById('basic-navbar');
+  ngOnInit(): void {
+    this.activeBtn('home-btn');
+  }
 
-    console.log();
+  displayMenu(): void {
+    this.displayedMenu = true;
+  }
+  hideMenu(): void {
+    this.displayedMenu = false;
+  }
+  activeBtn(id: string): void {
+    this.tabs.forEach((tab) => {
+      const btn = document.getElementById(tab.id);
+      btn?.classList.remove('active-btn');
+    });
+    const btn = document.getElementById(id);
+    btn?.classList.add('active-btn');
   }
 }
