d7493bca8c32340bea183818323d974d26cdd61a
Patrice-H <webmestre64@gmail.com>
2024-07-28 22:58:07 +0200

    Updates home page

--- src/app/page/home-page/home-page.component.ts
+++ src/app/page/home-page/home-page.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SkillsMenuComponent } from '../../component/skills-menu/skills-menu.component';
+import { SlideshowComponent } from '../../component/slideshow/slideshow.component';
+import { BadgesBoxComponent } from '../../component/badges-box/badges-box.component';
+import { ButtonRowComponent } from '../../component/button-row/button-row.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    SkillsMenuComponent,
+    SlideshowComponent,
+    BadgesBoxComponent,
+    ButtonRowComponent,
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  newContent!: number;
 
+  ngOnInit(): void {
+    this.newContent = 0;
+  }
+
+  changeContent(content: number) {
+    this.newContent = content;
+  }
 }
