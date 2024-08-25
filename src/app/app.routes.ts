a1ffa67f12b1db01ddb491d14a4c41e74f275c12
Patrice-H <webmestre64@gmail.com>
2024-08-25 19:25:36 +0200

    Removes unused import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomePageComponent } from './page/home-page/home-page.component';
 import { ContactPageComponent } from './page/contact-page/contact-page.component';
 import { AboutPageComponent } from './page/about-page/about-page.component';
