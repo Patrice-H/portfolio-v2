278ab52e6503948238674ccf43101e7cecb68d15
Patrice-H <webmestre64@gmail.com>
2024-09-02 21:48:00 +0200

    Updates component

--- src/app/component/main-menu/main-menu.component.ts
+++ src/app/component/main-menu/main-menu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { baseUrl } from '../../service/config';
 
 @Component({
   selector: 'app-main-menu',
@@ -10,39 +11,44 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss',
 })
-export class MainMenuComponent{
+export class MainMenuComponent implements OnInit{
   tabs: any[] = [
     {
       id: 'home-btn',
       link: '/',
       label: 'Accueil',
-      active: true,
-      page: 'home-page',
+      class: '',
     },
     {
       id: 'about-btn',
       link: '/about',
       label: 'A propos',
-      active: false,
-      page: 'about-page',
+      class: '',
     },
     {
       id: 'projects-btn',
       link: '/projects',
       label: 'Projets',
-      active: false,
-      page: 'projects-page',
+      class: '',
     },
     {
       id: 'contact-btn',
       link: '/contact',
       label: 'Contact',
-      active: false,
-      page: 'contact-page',
+      class: '',
     },
   ];
   displayedMenu: Boolean = false;
   disabledButton: Boolean = true;
+  location!: string;
+
+  ngOnInit(): void {
+    this.location = window.location.href.split(baseUrl)[1]
+    const rightIndex = (element: any) => element.link === this.location;
+    const index = this.tabs.findIndex(rightIndex);
+    this.tabs.forEach(tab => tab.class = '');
+    this.tabs[index].class = 'active-btn no-hover-effect';
+  }
 
   displayMenu(): void {
     this.displayedMenu = true;
